d0933652eea0c75239ff36751e1ba29702ee9a9b
Julio Cesar Bonon <julio.bonon@toradex.com>
2020-03-09 14:36:24 -0300

    Add SoMs variable

--- example/lib/UI/home.dart
+++ example/lib/UI/home.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:process_run/shell.dart';
 
 
-class HomePage extends StatelessWidget {
-  
-   Future get(moduleName) async {
 
+class HomePage extends StatelessWidget {
+
+  Future get(moduleName) async {
     var shell = Shell();
 
     shell = shell.pushd('/home/julio/projects/tezi/$moduleName');
@@ -13,201 +13,204 @@ class HomePage extends StatelessWidget {
     await shell.run('./recovery-linux.sh');
   }
 
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            padding: EdgeInsets.only(
-              top: 60,
-              right: 40,
-              left: 40,
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 60,
+          right: 40,
+          left: 40,
+        ),
+        color: Colors.blue[900],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              height: 100,
+              child: Image.asset('imgs/product-easy-installer-icon.png'),
             ),
-            color: Colors.blue[900],
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Container(
-                  height: 100,
-                  child: Image.asset('imgs/product-easy-installer-icon.png'),
-                ),
-                Container(
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        child: Row(
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Column(
                           crossAxisAlignment: CrossAxisAlignment.center,
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                Container(
-                                  child: Text(
-                                    'Apalis Family',
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 30),
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                ButtonTheme(
-                                  minWidth: 180,
-                                  child: RaisedButton(
-                                    color: Colors.white,
-                                    shape: new RoundedRectangleBorder(
-                                        borderRadius:
-                                            new BorderRadius.circular(10.0),
-                                        side: BorderSide(color: Colors.black)),
-                                    child: Text(
-                                      'Apalis iMX8',
-                                      style: TextStyle(
-                                          color: Colors.black, fontSize: 20),
-                                    ),
-                                    onPressed: () {},
-                                  ),
-                                ),
-                                ButtonTheme(
-                                  minWidth: 180,
-                                  child: RaisedButton(
-                                    color: Colors.white,
-                                    shape: new RoundedRectangleBorder(
-                                        borderRadius:
-                                            new BorderRadius.circular(10.0),
-                                        side: BorderSide(color: Colors.black)),
-                                    child: Text(
-                                      'Apalis iMX6',
-                                      style: TextStyle(
-                                          color: Colors.black, fontSize: 20),
-                                    ),
-                                    onPressed: () {}
-                                ),
-                                ),
-                                ButtonTheme(
-                                  minWidth: 180,
-                                  child: RaisedButton(
-                                    color: Colors.white,
-                                    shape: new RoundedRectangleBorder(
-                                        borderRadius:
-                                            new BorderRadius.circular(10.0),
-                                        side: BorderSide(color: Colors.black)),
-                                    child: Text(
-                                      'Apalis T30',
-                                      style: TextStyle(
-                                          color: Colors.black, fontSize: 20),
-                                    ),
-                                    onPressed: () => get('apalis-t30'),
-                                  ),
-                                ),
-                                ButtonTheme(
-                                  minWidth: 180,
-                                  child: RaisedButton(
-                                    color: Colors.white,
-                                    shape: new RoundedRectangleBorder(
-                                        borderRadius:
-                                            new BorderRadius.circular(10.0),
-                                        side: BorderSide(color: Colors.black)),
-                                    child: Text(
-                                      'Apalis TK1',
-                                      style: TextStyle(
-                                          color: Colors.black, fontSize: 20),
-                                    ),
-                                    onPressed: () {},
-                                  ),
-                                ),
-                              ],
+                            Container(
+                              child: Text(
+                                'Apalis Family',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 30),
+                              ),
                             ),
                             SizedBox(
-                              width: 30,
+                              height: 10,
                             ),
-                            Column(
-                              children: <Widget>[
-                                Container(
-                                  child: Text(
-                                    'Colibri Family',
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 30),
-                                  ),
+                            ButtonTheme(
+                              minWidth: 180,
+                              child: RaisedButton(
+                                color: Colors.white,
+                                shape: new RoundedRectangleBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(10.0),
+                                    side: BorderSide(color: Colors.black)),
+                                child: Text(
+                                  'Apalis iMX8',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 20),
                                 ),
-                                SizedBox(
-                                  height: 10,
+                                onPressed: () => get('apalis-imx8'),
+                              ),
+                            ),
+                            ButtonTheme(
+                              minWidth: 180,
+                              child: RaisedButton(
+                                color: Colors.white,
+                                shape: new RoundedRectangleBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(10.0),
+                                    side: BorderSide(color: Colors.black)),
+                                child: Text(
+                                  'Apalis iMX6',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 20),
                                 ),
-                                ButtonTheme(
-                                  minWidth: 180,
-                                  child: RaisedButton(
-                                    color: Colors.white,
-                                    shape: new RoundedRectangleBorder(
-                                        borderRadius:
-                                            new BorderRadius.circular(10.0),
-                                        side: BorderSide(color: Colors.black)),
-                                    child: Text(
-                                      'Colibri iMX8X',
-                                      style: TextStyle(
-                                          color: Colors.black, fontSize: 20),
-                                    ),
-                                    onPressed: () {},
-                                  ),
+                                onPressed: () => get('apalis-imx6'),
+                              ),
+                            ),
+                            ButtonTheme(
+                              minWidth: 180,
+                              child: RaisedButton(
+                                color: Colors.white,
+                                shape: new RoundedRectangleBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(10.0),
+                                    side: BorderSide(color: Colors.black)),
+                                child: Text(
+                                  'Apalis T30',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 20),
                                 ),
-                                ButtonTheme(
-                                  minWidth: 180,
-                                  child: RaisedButton(
-                                    color: Colors.white,
-                                    shape: new RoundedRectangleBorder(
-                                        borderRadius:
-                                            new BorderRadius.circular(10.0),
-                                        side: BorderSide(color: Colors.black)),
-                                    child: Text(
-                                      'Colibri iMX6',
-                                      style: TextStyle(
-                                          color: Colors.black, fontSize: 20),
-                                    ),
-                                    onPressed: () {},
-                                  ),
+                                onPressed: () => get('apalis-t30'),
+                              ),
+                            ),
+                            ButtonTheme(
+                              minWidth: 180,
+                              child: RaisedButton(
+                                color: Colors.white,
+                                shape: new RoundedRectangleBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(10.0),
+                                    side: BorderSide(color: Colors.black)),
+                                child: Text(
+                                  'Apalis TK1',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 20),
                                 ),
-                                ButtonTheme(
-                                  minWidth: 180,
-                                  child: RaisedButton(
-                                    color: Colors.white,
-                                    shape: new RoundedRectangleBorder(
-                                        borderRadius:
-                                            new BorderRadius.circular(10.0),
-                                        side: BorderSide(color: Colors.black)),
-                                    child: Text(
-                                      'Colibri iMX6ULL',
-                                      style: TextStyle(
-                                          color: Colors.black, fontSize: 20),
-                                    ),
-                                    
-                                    onPressed: () {},
-                                  ),
-                                ),
-                                ButtonTheme(
-                                  minWidth: 180,
-                                  child: RaisedButton(
-                                    color: Colors.white,
-                                    shape: new RoundedRectangleBorder(
-                                        borderRadius:
-                                            new BorderRadius.circular(10.0),
-                                        side: BorderSide(color: Colors.black)),
-                                    child: Text(
-                                      'Colibri iMX7',
-                                      style: TextStyle(
-                                          color: Colors.black, fontSize: 20),
-                                    ),
-                                    onPressed: () {},
-                                  ),
-                                ),
-                              ],
-                            )
+                                onPressed: () => get('apalis-tk1'),
+                              ),
+                            ),
                           ],
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Container(
+                              child: Text(
+                                'Colibri Family',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 30),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            ButtonTheme(
+                              minWidth: 180,
+                              child: RaisedButton(
+                                color: Colors.white,
+                                shape: new RoundedRectangleBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(10.0),
+                                    side: BorderSide(color: Colors.black)),
+                                child: Text(
+                                  'Colibri iMX8X',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 20),
+                                ),
+                                onPressed: () => get('colibri-imx8x'),
+                              ),
+                            ),
+                            ButtonTheme(
+                              minWidth: 180,
+                              child: RaisedButton(
+                                color: Colors.white,
+                                shape: new RoundedRectangleBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(10.0),
+                                    side: BorderSide(color: Colors.black)),
+                                child: Text(
+                                  'Colibri iMX6',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 20),
+                                ),
+                                onPressed: () => get('colibri-imx6'),
+                              ),
+                            ),
+                            ButtonTheme(
+                              minWidth: 180,
+                              child: RaisedButton(
+                                color: Colors.white,
+                                shape: new RoundedRectangleBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(10.0),
+                                    side: BorderSide(color: Colors.black)),
+                                child: Text(
+                                  'Colibri iMX6ULL',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 20),
+                                ),
+                                onPressed: () => get('colibri-imx6ull'),
+                              ),
+                            ),
+                            ButtonTheme(
+                              minWidth: 180,
+                              child: RaisedButton(
+                                color: Colors.white,
+                                shape: new RoundedRectangleBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(10.0),
+                                    side: BorderSide(color: Colors.black)),
+                                child: Text(
+                                  'Colibri iMX7',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 20),
+                                ),
+                                onPressed: () => get('colibri-imx7'),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            )));
+                ],
+              ),
+            ),
+        
+          ],
+        ),
+      ),
+    );
   }
 }
